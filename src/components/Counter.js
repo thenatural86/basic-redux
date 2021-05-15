863459d29c6eda2ef040a93e7cf934e541ea8040
thenatural86 <philip.smalls86@gmail.com>
2021-05-15 10:50:49 -0400

    dispatch actions using redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import classes from './Counter.module.css'
+import { counterActions } from '../store/index'
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter)
@@ -7,19 +8,19 @@ const Counter = () => {
   const dispatch = useDispatch()
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   }
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', payload: 5 })
+    dispatch(counterActions.increase(5))
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   }
-  // change
+
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
   }
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -25,36 +25,5 @@ const store = configureStore({
   reducer: counterSlice.reducer,
 })
 
+export const counterActions = counterSlice.actions
 export default store
-
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === 'increment') {
-//     return {
-//       ...state,
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     }
-//   }
-
-//   if (action.type === 'increase') {
-//     return {
-//       ...state,
-//       counter: state.counter + action.payload,
-//       showCounter: state.showCounter,
-//     }
-//   }
-
-//   if (action.type === 'decrement') {
-//     return {
-//       ...state,
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     }
-//   }
-
-//   if (action.type === 'toggle') {
-//     return { ...state, counter: state.counter, showCounter: !state.showCounter }
-//   }
-
-//   return state
-// }
