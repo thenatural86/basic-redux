fc5404483c0224af830d90a7ff637b29f77368a7
thenatural86 <philip.smalls86@gmail.com>
2021-05-15 10:41:19 -0400

    configureStore from redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
-import { createStore } from 'redux'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
 
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -22,38 +21,40 @@ createSlice({
   },
 })
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    }
-  }
-
-  if (action.type === 'increase') {
-    return {
-      ...state,
-      counter: state.counter + action.payload,
-      showCounter: state.showCounter,
-    }
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      ...state,
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    }
-  }
-
-  if (action.type === 'toggle') {
-    return { ...state, counter: state.counter, showCounter: !state.showCounter }
-  }
-
-  return state
-}
-
-const store = createStore(counterReducer)
+const store = configureStore({
+  reducer: counterSlice.reducer,
+})
 
 export default store
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return {
+//       ...state,
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     }
+//   }
+
+//   if (action.type === 'increase') {
+//     return {
+//       ...state,
+//       counter: state.counter + action.payload,
+//       showCounter: state.showCounter,
+//     }
+//   }
+
+//   if (action.type === 'decrement') {
+//     return {
+//       ...state,
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     }
+//   }
+
+//   if (action.type === 'toggle') {
+//     return { ...state, counter: state.counter, showCounter: !state.showCounter }
+//   }
+
+//   return state
+// }
