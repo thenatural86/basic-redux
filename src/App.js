d47e5fea1a61a5d07c7c16c359526f05e1188eb1
thenatural86 <philip.smalls86@gmail.com>
2021-05-16 11:14:10 -0400

    render auth and header compoments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Counter from './components/Counter';
-
+import Counter from './components/Counter'
+import Header from './components/Header'
+import Auth from './components/Auth'
 
 function App() {
   return (
-    <Counter />
-  );
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
+  )
 }
 
-export default App;
+export default App
