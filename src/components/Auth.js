aa0551a07067d55da13c71d0f00f6cb7820de6d2
thenatural86 <philip.smalls86@gmail.com>
2021-05-16 11:54:32 -0400

    conditional render

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -23,7 +23,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          {!isAuthenticated && <button onClick={loginUser}>Login</button>}
+          <button onClick={loginUser}>Login</button>
         </form>
       </section>
     </main>
